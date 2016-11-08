a325f3845e4a9fcea5950402e7577b70ac4630c2
grzala <m.panasiuk.14@outlook.com>
2016-11-08 17:32:49 +0000

    algorithm returns values between -1.0 and 1.0

--- ReccomenderAlgorithm/src/reccomenderalgorithm/ReccomenderAlgorithm.java
+++ ReccomenderAlgorithm/src/reccomenderalgorithm/ReccomenderAlgorithm.java
@@ -8,6 +8,8 @@ package reccomenderalgorithm;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -25,28 +27,31 @@ public class ReccomenderAlgorithm {
        System.out.println("comparing for " + usr.name);
        
        for (int i = 1; i < fd.users.length; i++) {
+        ArrayList<Interest> likes1 = new ArrayList<Interest>(usr.interests);
+        ArrayList<Interest> likes2 = new ArrayList<Interest>(fd.users[i].interests);
+        ArrayList<Interest> dislikes1 = new ArrayList<Interest>(Arrays.asList(fd.interests)); dislikes1.removeAll(likes1);
+        ArrayList<Interest> dislikes2 = new ArrayList<Interest>(Arrays.asList(fd.interests)); dislikes2.removeAll(likes2);
+          
+        ArrayList<Interest> likes1likes2intersection = new ArrayList<Interest>(likes1); likes1likes2intersection.retainAll(likes2);
+        ArrayList<Interest> dislikes1dislikes2intersection = new ArrayList<Interest>(dislikes1); dislikes1dislikes2intersection.retainAll(dislikes2);
+        ArrayList<Interest> likes1dislikes2intersection = new ArrayList<Interest>(likes1); likes1dislikes2intersection.retainAll(dislikes2);
+        ArrayList<Interest> dislikes1likes2intersection = new ArrayList<Interest>(dislikes1); dislikes1likes2intersection.retainAll(likes2);
         
-        //similar interests
-        ArrayList<Interest> similarInterests = new ArrayList<Interest>(usr.interests);
-        similarInterests.retainAll(fd.users[i].interests);
-        
-        //similar non-interests (raquetball hate)
-        ArrayList<Interest> user1Dislikes = new ArrayList<Interest>(Arrays.asList(fd.interests));
-        ArrayList<Interest> user2Dislikes = new ArrayList<Interest>(Arrays.asList(fd.interests));
-        ArrayList<Interest> dislikes = new ArrayList<Interest>(Arrays.asList(fd.interests));
-        user1Dislikes.removeAll(usr.interests);
-        user2Dislikes.removeAll(fd.users[i].interests);
-        dislikes.removeAll(usr.interests); dislikes.removeAll(fd.users[i].interests);
+        Set<Interest> set = new HashSet<Interest>();
+        set.addAll(likes1likes2intersection);
+        set.addAll(dislikes1dislikes2intersection);
+        set.addAll(likes1dislikes2intersection);
+        set.addAll(dislikes1likes2intersection);
+        ArrayList<Interest> unionAll = new ArrayList<Interest>(set);
         
         
         System.out.println(fd.users[i].name);
         float matchCoefficient;
-        matchCoefficient = ((float)similarInterests.size() + (float)dislikes.size()) / (float)(usr.interests.size() + fd.users[i].interests.size() + (float)user1Dislikes.size() + (float)user2Dislikes.size());
+        matchCoefficient = likes1likes2intersection.size() + dislikes1dislikes2intersection.size();
+        matchCoefficient = matchCoefficient - likes1dislikes2intersection.size() - dislikes1likes2intersection.size();
+        matchCoefficient /= unionAll.size();
         System.out.println(matchCoefficient);
-        
                
-       }
-       
+       }  
     }
-    
 }

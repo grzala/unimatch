d14b7b717340214bc82a8ec0b9725cdf94ee54d1
grzala <m.panasiuk.14@outlook.com>
2016-11-08 16:49:30 +0000

    set test database to unimatch.ddns.net mysql

--- unimatch_rails/db/schema.rb
+++ unimatch_rails/db/schema.rb
@@ -12,16 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20161101155405) do
 
-  create_table "billing_histories", force: :cascade do |t|
+  create_table "billing_histories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.date     "date"
-    t.float    "amount"
+    t.float    "amount",     limit: 24
     t.integer  "society_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["society_id"], name: "index_billing_histories_on_society_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.index ["society_id"], name: "index_billing_histories_on_society_id", using: :btree
   end
 
-  create_table "events", force: :cascade do |t|
+  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.date     "date"
     t.time     "time"
@@ -29,66 +29,66 @@ ActiveRecord::Schema.define(version: 20161101155405) do
     t.string   "location"
     t.boolean  "fixed"
     t.boolean  "frequency"
-    t.float    "cost"
+    t.float    "cost",        limit: 24
     t.integer  "society_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["society_id"], name: "index_events_on_society_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["society_id"], name: "index_events_on_society_id", using: :btree
   end
 
-  create_table "interest_groups", force: :cascade do |t|
+  create_table "interest_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "interests", force: :cascade do |t|
+  create_table "interests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "interest_group_id"
     t.string   "name"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.index ["interest_group_id"], name: "index_interests_on_interest_group_id"
+    t.index ["interest_group_id"], name: "index_interests_on_interest_group_id", using: :btree
   end
 
-  create_table "members", force: :cascade do |t|
+  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.integer  "user_id"
     t.integer  "society_id"
     t.boolean  "admin",      default: false
-    t.index ["society_id"], name: "index_members_on_society_id"
-    t.index ["user_id"], name: "index_members_on_user_id"
+    t.index ["society_id"], name: "index_members_on_society_id", using: :btree
+    t.index ["user_id"], name: "index_members_on_user_id", using: :btree
   end
 
-  create_table "societies", force: :cascade do |t|
+  create_table "societies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.text     "description"
-    t.float    "cost"
+    t.text     "description", limit: 65535
+    t.float    "cost",        limit: 24
     t.boolean  "paid"
     t.boolean  "recurring"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table "universities", force: :cascade do |t|
+  create_table "universities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "city"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_universities_on_user_id"
+    t.index ["user_id"], name: "index_universities_on_user_id", using: :btree
   end
 
-  create_table "user_interests", force: :cascade do |t|
+  create_table "user_interests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.integer  "interest_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["interest_id"], name: "index_user_interests_on_interest_id"
-    t.index ["user_id"], name: "index_user_interests_on_user_id"
+    t.index ["interest_id"], name: "index_user_interests_on_interest_id", using: :btree
+    t.index ["user_id"], name: "index_user_interests_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
     t.string   "name"
     t.string   "surname"

49c0f25d3bf5dfecac729b3fde7583ce0b2f45f6
grzala <m.panasiuk.14@outlook.com>
2017-01-23 18:37:13 +0000

    society form working

--- unimatch_rails/app/controllers/event_controller.rb
+++ unimatch_rails/app/controllers/event_controller.rb
@@ -1,28 +1,26 @@
 class EventController < ApplicationController
     
     def new
-        @event = Event.new
     end
     
     def create
-        @event = Event.new()
+        @event = Event.new
         @event.name = params[:name]
         @event.description = params[:description]
         @event.location = params[:location]
         @event.cost = params[:cost]
         #@event.recurring = params[:recurring]
-        @event.name = params[:name]
-        @event.startdate = Date.parse(params[:startdate])
-        @event.enddate = Date.parse(params[:enddate])
+        @event.startdate = Date.parse(params[:startdate]).strftime("%Y-%m-%d")
+        @event.enddate = Date.parse(params[:enddate]).strftime("%Y-%m-%d")
         @event.time = (params[:hour].to_s + params[:minute].to_s).to_i
         
         
         
         
         
-        @event.save
+        if !@event.save then puts @event.errors.full_messages end
         
-        redirect_to root_path
+        #redirect_to root_path
     end
     
 

6d8fdce113482f1708fb0876d507173716c1d0cb
grzala <m.panasiuk.14@outlook.com>
2017-02-09 13:45:19 +0000

    unique interests

--- unimatch_rails/db/seeds/interests.seeds.rb
+++ unimatch_rails/db/seeds/interests.seeds.rb
@@ -7,13 +7,6 @@ interests_yaml.each do |group, interests|
     @i.save
     interests.each do |interest|
        @interest = Interest.create(name: interest, interest_group_id: @i.id)
-       if !@interest.valid? 
-            puts "ERROR CREATING INTERESTS"
-            puts @interest.errors.count.to_s + " error"
-            puts @interest.errors.full_messages
-            puts "REMEMBER TO DROP DB BEFORE RESEEDING"
-            break
-        end
     end
 end
 

4feeff3a6d740c7b2aeb2630ee0b8baaa22213c2
grzala <m.panasiuk.14@outlook.com>
2017-02-16 16:03:10 +0000

    interests seed repaired

--- unimatch_rails/db/seeds/interests.seeds.rb
+++ unimatch_rails/db/seeds/interests.seeds.rb
@@ -3,8 +3,9 @@ puts 'creating interests...'
 interests_yaml = YAML.load_file('db/seeds/yaml/interests.yaml')
 
 interests_yaml.each do |group, interests|
-    @i = InterestGroup.new(name: group)
-    @i.save
+    @i = InterestGroup.find_by_name(group)
+    if @i == nil then @i = InterestGroup.create(name: group) end
+        
     interests.each do |interest|
        @interest = Interest.create(name: interest, interest_group_id: @i.id)
     end

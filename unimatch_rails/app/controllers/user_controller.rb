73a2f150887fa5dfa8f1b0fb8552fc3f4b760961
muzzab <j.murraybrown.14@aberdeen.ac.uk>
2017-02-19 01:09:46 +0000

    added styling to alerts. they are now 2 different alerts. success and warning

--- unimatch_rails/app/controllers/user_controller.rb
+++ unimatch_rails/app/controllers/user_controller.rb
@@ -32,16 +32,16 @@ class UserController < ApplicationController
     @user.save
     
     if @user.save
-      flash[:notice] = "Account created"
-      redirect_to :controller => :session, action => :new
+      flash[:success] = "Account created successfuly. Please log in."
+      redirect_to :controller => :session, :action => :new
     elsif (:password) != (:password_confirmation)
-      flash[:notice] = "Passwords are different"
-      puts flash[:notice]
-      redirect_to :controller => :session, action => :new
+      flash[:warning] = "Passwords are different"
+      puts flash[:warning]
+      redirect_to :controller => :session, :action => :new
         
     else
-      flash[:notice] = "Account not created"
-      puts flash[:notice]
+      flash[:warning] = "Account not created"
+      puts flash[:warning]
       redirect_to :controller => :session, action => :new
     end
     

e6c041cbf6a254b62032e41607f142020e8cc2c2
grzala <m.panasiuk.14@outlook.com>
2017-02-21 15:24:15 +0000

    fix login form flashing bug

--- unimatch_rails/app/assets/javascripts/login.js
+++ unimatch_rails/app/assets/javascripts/login.js
@@ -8,15 +8,15 @@ $(document).on('turbolinks:load', function() {
    $(function() {
 
     $('#login_title').click(function(e) {
-		$("#login_form").delay(99).fadeIn(100);
- 		$("#register_form").fadeOut(100);
+ 		$("#register_form").hide();
+		$("#login_form").fadeIn(100);
  		$('#register_text').removeClass('active');
 		$('#login_text').addClass('active');
 		e.preventDefault();
 	});
 	$('#register_title').click(function(e) {
-		$("#register_form").delay(99).fadeIn(100);
- 		$("#login_form").fadeOut(100);
+ 		$("#login_form").hide();
+		$("#register_form").fadeIn(100);
  		$('#login_text').removeClass('active');
 		$('#register_text').addClass('active');
 		e.preventDefault();

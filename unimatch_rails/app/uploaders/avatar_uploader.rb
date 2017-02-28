343786ea4c7d1c32b2e9a31ae97df22ab585a709
muzzab <j.murraybrown.14@aberdeen.ac.uk>
2017-02-28 14:45:37 +0000

    default image if you dont upload one

--- unimatch_rails/app/uploaders/avatar_uploader.rb
+++ unimatch_rails/app/uploaders/avatar_uploader.rb
@@ -3,7 +3,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   
   def default_url(*args)
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    def default_url(*args)
+      ActionController::Base.helpers.asset_path("assets/" + [version_name, "default.png"].compact.join('_'))
+    end
   end
   
   

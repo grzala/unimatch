0e4abd891cd63d234c3d125e45b4b8dae53627e9
grzala <m.panasiuk.14@outlook.com>
2017-03-20 13:27:00 +0000

    toggle

--- unimatch_rails/app/controllers/application_controller.rb
+++ unimatch_rails/app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class ApplicationController < ActionController::Base
   #connect to algorithm
   include Connect
   
+  before_action :get_user_image
+  
+  def get_user_image
+    if !session[:user_id].nil?
+      user = User.find(session[:user_id])
+      @image_thumbnail = user.avatar_url(:display)
+    end
+  end
   
 end

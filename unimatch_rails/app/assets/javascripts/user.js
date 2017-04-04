8d448e6e94b233fb6a29022ac2fe525b315aba7b
grzala <m.panasiuk.14@outlook.com>
2017-04-04 12:15:36 +0000

    favourite users list and removing

--- unimatch_rails/app/assets/javascripts/user.js
+++ unimatch_rails/app/assets/javascripts/user.js
@@ -1,6 +1,7 @@
 /* global $ */
 
 $(document).ready(function() {
+    //star
     $(".star").click(function(e) {
         var star = this
         switch_favourite($(star).attr("user_id"))
@@ -12,6 +13,15 @@ $(document).ready(function() {
             $(star).removeClass("star-inactive");
             $(star).addClass("star-active");
         }
+    })
+    
+    //hover on mini-userbox
+    $(".user-mini-container .user-mini-element").mouseenter(function() {
+        $(this).find(".special-action").show();
+    })
+    
+    $(".user-mini-container .user-mini-element").mouseleave(function() {
+        $(this).find(".special-action").hide();
     })
 });
 
